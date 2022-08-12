1ad210711bafc3ad6a171effb632882528424262
grazomarin <huseynovk355@gmail.com>
2022-08-12 22:56:46 +0400

    Added mobile support, animations, changed design
    In Mobile support now the navigation bar collapses when reaching a
    certain width and main elements become blocks. Animation on scroll were
    added "in mobile mode animations are too slow". Button style was
    changed, font was changed, text size, padding, image size, elements size
    now are responsive to the viewport width

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const headLinks = document.querySelectorAll('.hLink')
+const navIcon = document.querySelector('.mobileNavIcon')
+const menuBtn = document.querySelector('.menuBtn')
+const modal = document.querySelector('.modal')
+let menuOpen = false;
 
 headLinks.forEach(link => {
     link.addEventListener('click', () => {
@@ -6,3 +10,15 @@ headLinks.forEach(link => {
         link.classList.add('active')
     })
 });
+
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        modal.classList.add('active')
+        menuBtn.classList.add('open')
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open')
+        menuOpen = false;
+        modal.classList.remove('active')
+    }
+})
